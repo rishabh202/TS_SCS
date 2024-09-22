9c74684b0ffd037c4e5061f1f6239d5d0d5e0e83
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-22 11:11:29 +0530

    intialised a TS project

---
+++ 1/app.ts
@@ -0,0 +1,2 @@
+// The settings or rules of syntax of the TS file is defined in tsconfig.json file
+// Setting up a TS project is as easy as creating a TS file and then creating tsconfig.json by the cmd tsc --init or tsc.cmd --init
