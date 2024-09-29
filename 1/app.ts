90440e33721c7f6f1c14b171d3c8dbe785ee9ce8
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-29 11:06:33 +0530

    learning

--- 1/app.ts
+++ 1/app.ts
@@ -1,2 +1,6 @@
 // The settings or rules of syntax of the TS file is defined in tsconfig.json file
 // Setting up a TS project is as easy as creating a TS file and then creating tsconfig.json by the cmd tsc --init or tsc.cmd --init
+
+
+// basic types, primitve and non primitve (Reference) etc
+let a = 256;
