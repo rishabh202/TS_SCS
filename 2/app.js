a8e06bdc69055840d258de8ef89d11954df251c3
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-29 11:18:40 +0530

    done basic with arrays

---
+++ 2/app.js
@@ -0,0 +1,6 @@
+// arrays 
+let a = [1, 2, 'harish', 'ramesh', 369, { rollno: 21 }];
+console.log(a);
+
+// let arr: number[] = [23 ,425];   
+// see the above is valid in ts but not in js
--- 2/app.ts
+++ 2/app.ts
@@ -1,1 +1,7 @@
 // arrays 
+let a = [1, 2, 'harish', 'ramesh', 369, {rollno: 21}];
+console.log(a);
+
+// let arr: number[] = [2,89,56, 'rus'];  //throws error obv
+
+let arr: number[] = [23 ,425,];  
