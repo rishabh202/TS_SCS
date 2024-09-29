e9538c716879874fda2f47a7ec591f7b63a11370
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-29 11:30:20 +0530

    tuples done

---
+++ 3/app.ts
@@ -0,0 +1,9 @@
+// Tuples
+// Js does not have tuples
+
+let arr: [number, string] = [369, 'shukla'];
+// let arr1: [string, object] = ['shukla' ]; // error
+let arr2: [number, object] = [369, {surname: 'shukla'}];
+// let arr3: [number, string] = [369, 'shukla', 369];  // only 2 elements can come
+// let arr4: [number, string] = ['shukla', 369]; // obv error 
+
