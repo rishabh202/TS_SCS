797b710662bf5752e8dd767b6ab60a4cfb167639
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-01 10:02:13 +0530

    understanding type inference and type annotations

---
+++ 6/app.ts
@@ -0,0 +1,21 @@
+// Type inference (grammar: An inference is an idea or conclusion that's drawn from evidence and reasoning)
+// understanding type inference
+// Type annotations
+
+let a: number;
+let b = 65;  // This is called inference. automatically sets the datatype, by observing the data
+let c = "36";
+
+// Annoations simply means ki "what" like for eg
+let x: number | string | boolean;
+x = 89;
+x = "69";
+x = true;
+
+// Above type annotation was on variable. Below is annotation on functions
+function abcd(a: number, b: string):void{
+
+};
+
+
+
