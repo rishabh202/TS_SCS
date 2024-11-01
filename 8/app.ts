0ac857a5f339c335c8985e45f28bd39e50a81874
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-01 20:24:12 +0530

    done extending interfaces

--- 8/app.ts
+++ 8/app.ts
@@ -1,3 +1,33 @@
 // Extending interfaces
+
+interface User {
+    name: string,
+    password: number,
+    email: string
+}
+interface Admin extends User {
+    admin: boolean;
+}
+
+function abc(obj: Admin) {
+    // obj. 
+}
+
+
+// see two interfaces of the same name are merged and one interface is formed of them. It does not overwrites but merges
+interface xyz {
+    name: string;
+}
+interface xyz {
+    email: string;
+}
+
+
+function a(obj: xyz) {
+    // obj.  
+}
+
+
 // Type aliases
 // Intersection types 
+
