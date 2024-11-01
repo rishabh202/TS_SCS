05585025de48beec99dccb57a26ba33f22552b47
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-01 22:52:43 +0530

    introduction to classes

---
+++ 9/app.js
@@ -0,0 +1,20 @@
+// classes & objects
+// class definition 
+// constructions 
+// Access modifiers (public, private, protected)
+// readonly properties 
+// optional properties 
+// parameter properties 
+// Getters and setters 
+// Static members 
+// Abstract classes and methods 
+var Device = /** @class */ (function () {
+    function Device() {
+        this.name = 'lg';
+        this.price = 12000;
+        this.category = "digital";
+    }
+    return Device;
+}());
+var d1 = new Device();
+var d2 = new Device();
---
+++ 9/app.ts
@@ -0,0 +1,21 @@
+// classes & objects
+// class definition 
+// constructions 
+// Access modifiers (public, private, protected)
+// readonly properties 
+// optional properties 
+// parameter properties 
+// Getters and setters 
+// Static members 
+// Abstract classes and methods 
+
+
+class Device{
+    name = 'lg';
+    price = 12000;
+    category = "digital"
+}
+
+
+let d1 = new Device();
+let d2 = new Device();
