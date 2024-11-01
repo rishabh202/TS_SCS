a3f886dc1b3886438462429ba3082e2bb65eb7c5
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-01 20:48:57 +0530

    intro to type alias and type intersection

--- 8/app.ts
+++ 8/app.ts
@@ -28,6 +28,52 @@ function a(obj: xyz) {
 }
 
 
-// Type aliases
-// Intersection types 
+// Introduction to Type aliases
 
+type sankhya = number; 
+let p: sankhya;
+
+
+type value = string | number | null;
+let q: value;
+
+function we(obj: value){
+
+}
+
+we("ram");
+we(6);
+// we(true);   // error obv
+we(null);
+
+
+
+// Intersection types 
+type Employee = {
+    name: string,
+    email: string
+}
+
+type HR =  Employee & {
+    getDetails(user: string): void
+}
+
+function interview(o: HR){
+    // o.
+}
+
+
+// Difference between interfaces and types?
+// First difference is that u cant merge types unlike interfaces
+
+// type po = string; 
+// type po = number;   Error unlikes interfaces
+
+interface po{
+    name: string;
+}
+interface po{
+    age: number;
+}
+
+// another difference is that ki type deals with data types and interface deals with creating the shape of the object.
