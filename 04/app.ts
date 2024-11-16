25346cccb085d628245e3fa8e4d4f7274b9a2d36
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-16 11:47:22 +0530

    enums in TS

--- 04/app.ts
+++ 04/app.ts
@@ -1,7 +1,6 @@
 // Enums or Enumerations
 // JavaScript doesn’t natively support enums like TypeScript does. However, similar behavior can be mimicked using objects.
 
-
 enum statusCodes {
     ABONDED = 'abonded status codes 500', 
     NOTFOUND = 'not found status codes 404'
