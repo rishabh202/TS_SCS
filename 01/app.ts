6c224175e70eb9259a3793e03adf96021f60e7c7
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-16 11:41:18 +0530

    setting up env and introduction to basic types

--- 01/app.ts
+++ 01/app.ts
@@ -2,5 +2,6 @@
 // Setting up a TS project is as easy as creating a TS file and then creating tsconfig.json by the cmd tsc --init or tsc.cmd --init
 
 
+
 // basic types, primitve and non primitve (Reference) etc
 let a = 256;
