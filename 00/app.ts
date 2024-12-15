182b5e0aaaca30301b7980684d5853901c2f9a38
Rishabh Shukla <72446458+rishabh202@users.noreply.github.com>
2024-12-15 13:19:19 +0530

    setting up environment and installing ts and syllabus

--- 00/app.ts
+++ 00/app.ts
@@ -1,5 +1,89 @@
 var a = 12; 
 
+// https://www.youtube.com/watch?v=lVzb6pmel_E&t=247s&pp=ygUVdHlwZXNjcmlwdCBkb21paW5hdGlv
+// course link above.
+
+
+//
+// 0:00 - 04:06 - Intro 
+// 04:07 - 07:15 - Setting up 
+// Environment for TypeScript 
+// 07:16 - 10:22 - Installing
+//  TypeScript
+// 10:23 - 12:10 - Installing
+//  Essential VS Code 
+// Extensions 
+// 12:11 - 21:05 - Setting Up & Compiling TypeScript Project
+// 21:06 - 31:12 - Introduction 
+// to 
+// Basic Types 
+// 31:13 - 34:08 - 
+// Primitive Types  
+// 34:09 - 37:21 - 
+// Arrays 
+// 37:22 - 40:20 -
+//  Tuples
+// 40:21 - 45:17 -  Enums
+// 45:18 - 55:39 - Any, Unknown, 
+// Void & More
+// 55:40 - 57:37 - Introduction 
+// to 
+// Type Inference & Type Annotations  
+// 57:38 - 58:35 - Type Annotations
+// 58:36 - 01:10:55 - Introduction 
+// to 
+// Interfaces & Type Aliases
+// 01:10:56 - 01:15:04 - Extending Interfaces 
+// 01:15:05 - 01:17:58 - Fundamentals 
+// of
+// Type Aliases 
+
+
+   
+// 01:17:59 - 01:22:09 - Intersection
+// Types 
+// 01:22:10 - 01:23:11 - Introduction 
+// to 
+// Classes & Objects
+// 01:23:12 - 01:27:43 - Fundamental 
+// of
+// Classes & Objects 
+// 01:27:44 - 01:54:21 - Classes & Objects :
+//  Constructor
+// 01:54:22 - 02:02:38 - Classes & Objects : 
+// this. keyword
+ 
+// 02:02:39 - 02:13:24 - Classes & Objects : 
+// Public & Private 
+// Access Modifier
+// 02:13:25 - 02:19:12 - Classes & Objects :
+//  Protected
+//  Access Modifier
+// 02:19:13 - 02:22:05 - Classes & Objects : 
+// Optional Properties
+ 
+// 02:22:05 - 02:26:08 - Classes & Objects :
+//  Parameter Properties
+// 02:26:09 - 02:34:23 - Classes & Objects :
+//  Getters & Setters
+// 02:34:24 - 02:38:27 - Classes & Objects : 
+// Static Members  
+// 02:38:28 - 02:50:09 - Classes & Objects :
+//  Abstract Classes
+// 02:50:10 - 03:03:04 - Introduction
+//  to 
+// Functions
+// 03:03:05 - 03:10:07 - Functions
+//  : Rest Parameter
+// 03:10:08 - 03:16:02 - Functions
+//  : Overloading
+// 03:16:03 - 03:37:20 - Generics
+// 03:37:21 - 03:41:33 - Modules 
+// 03:41:34 - 03:47:20 - Type 
+// Assertions
+// 03:47:21 - 03:55:58 - Type 
+// Guards 
+// 03:55:59 - 03:57:38 - Upcoming Stuff !!
 
 a = "Rishabh";
 
